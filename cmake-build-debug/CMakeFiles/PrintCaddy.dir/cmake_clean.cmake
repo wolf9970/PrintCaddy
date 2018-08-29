60c42252fc01c3a0ec4654bcdd0261b9676e3242
wolf9970 <38161200+wolf9970@users.noreply.github.com>
2018-08-29 11:48:07 -0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/PrintCaddy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrintCaddy.dir/main.cpp.o"
+  "PrintCaddy.pdb"
+  "PrintCaddy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrintCaddy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
